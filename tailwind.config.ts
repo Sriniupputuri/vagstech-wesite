23643c5a8bc71025e970268f4a090308ae00015e
Sriniupputuri <srinivasupputuri9999@gmail.com>
2026-05-18 21:20:28 +0530

    updated with logo and theme

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -9,6 +9,7 @@ const config: Config = {
   ],
   theme: {
     extend: {
+      // placeholderColor: ['focus'],
       backgroundImage: {
         'gradient-radial': 'radial-gradient(var(--tw-gradient-stops))',
         'gradient-conic':
